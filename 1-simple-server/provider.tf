e6b44de3c658d2c81f22e229703286a84df7544f
Laba Subagia <labasubagia22@gmail.com>
2024-05-18 12:18:59 +0800

    provider meta

--- 1-simple-server/provider.tf
+++ 1-simple-server/provider.tf
@@ -1,5 +1,21 @@
+terraform {
+  required_version = "~> 1.6.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.29.0"
+    }
+  }
+}
+
 # GCP Provider
 provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+
+provider "google-beta" {
   project = var.gcp_project_id
   region  = var.gcp_region
   zone    = var.gcp_zone
